bdca6545403d185030903143732e9fb444189c43
jaehyeok <wogur0309@naver.com>
2023-11-08 15:15:40 +0900

    스프링 초기 설정완료

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello.......";
+		
+	}
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("pageName", "about");
+		return "home";
+	}
+	
+
+}
